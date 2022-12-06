04191f83c59e8b539deffecf4cb3ed999c836342
colinpereira <64594861+colinpereira@users.noreply.github.com>
2022-12-05 19:42:14 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import "./App.css";
+import react, { useState } from "react";
+import Background from "./images/background.jpeg";
+import funnyOlga from "./funnyOlga";
+import brugBooze from "./images/drugBooze.png";
+import gang from "./images/gang.png";
+import vegan from "./images/vegan.png";
+
+function App() {
+  const crosswords = [
+    "Garden Party Gang",
+    "Familia turned druggie and boozie",
+    "The World of Veganism",
+    "Music to your ears",
+  ];
+  const [whichClicked, setWhichClicked] = useState(0);
+  const [clicked, setClicked] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [alert, setAlert] = useState(false);
+
+  const randomImage = funnyOlga[Math.floor(Math.random() * funnyOlga.length)];
+
+  const handleCrosswordButton = () => {
+    setAlert(true);
+    setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+  };
+
+  if (!clicked) {
+    if (!alert) {
+      return (
+        <div
+          className="mainContainer"
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="App">
+            <div className="headerContainer">
+              <h1>SECRET SANTA 2022</h1>
+              <h2>WELCOME TO YOUR ANSWERS OLGALICIOUS!</h2>
+              <h2>CLICK A BUTTON TO FIND THE ANSWER KEY</h2>
+            </div>
+            <div className="buttonContainer">
+              {crosswords.map((c, i) => {
+                return <button onClick={handleCrosswordButton}>{c}</button>;
+              })}
+              {alert && (
+                <div className="alert-container">
+                  <div className="alert-inner">Alert! Alert!</div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="mainContainer"
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="App">
+            <div className="imageContainer">
+              <img src={randomImage} />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div
+        className="mainContainer"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="App">
+          <h1>ANSWERS</h1>
+          <br />
+          <img src={gang} />
+          <div className="buttonContainer">
+            <button
+              onClick={() => {
+                setClicked(false);
+              }}
+            >
+              Go Back
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/funnyOlga.js
@@ -0,0 +1,12 @@
+const funnyOlga = [
+  require("./funnyOlga/1.JPG"),
+  require("./funnyOlga/2.JPG"),
+  require("./funnyOlga/3.jpg"),
+  require("./funnyOlga/4.jpg"),
+  require("./funnyOlga/5.PNG"),
+  require("./funnyOlga/6.jpg"),
+  require("./funnyOlga/7.jpg"),
+  require("./funnyOlga/8.JPG"),
+];
+
+export default funnyOlga;
